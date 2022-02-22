56bff11ab58d7bccab4382509c90c45ff07c0c6e
Toby <40370496+DaatUserName@users.noreply.github.com>
2022-02-22 22:11:10 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    printMe();
+  })
+}
